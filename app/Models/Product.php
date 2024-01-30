bccdd4151c7214cdc1a5633c5ae208085192e7fc
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 07:16:34 +0330

    doc: add dock type for product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use MongoDB\Laravel\Eloquent\Model;
 use MongoDB\Laravel\Eloquent\SoftDeletes;
 
+/**
+ * @property string $name
+ * @property integer $price
+ * @property integer $inventory
+ */
 class Product extends Model
 {
     use HasFactory;
