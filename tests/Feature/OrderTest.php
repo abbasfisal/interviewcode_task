494ad5bb8ac15c24fa32b9e8ad1a3def246fbc4e
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 18:33:03 +0330

    feat: add test unauthenticated in order test

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -2,9 +2,26 @@
 
 namespace Tests\Feature;
 
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Tests\TestCase;
 
 class OrderTest extends TestCase
 {
+    public function clearProducts(): void
+    {
+        User::query()->forceDelete();
+        Product::query()->forceDelete();
+        Order::query()->forceDelete();
+    }
+
+
+    public function test_unauthenticated()
+    {
+        $this->clearProducts();
+        $this->getJson(route('orders.index'))
+            ->assertUnauthorized();
+    }
 
 }
