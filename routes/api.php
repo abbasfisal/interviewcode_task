93862666ec580cae55df77924ba010446e785f3e
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 09:41:35 +0330

    feat: add login,register to api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticateController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/login', [AuthenticateController::class, 'login'])->name('login');
+Route::post('/register', [AuthenticateController::class, 'register'])->name('register');
 Route::apiResource('products', ProductController::class);
