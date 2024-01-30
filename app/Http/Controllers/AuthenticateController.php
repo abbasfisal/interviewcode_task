782d901435772bdb89ebe560678a03e877bf2155
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 09:57:06 +0330

    fix: unique rule

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticateController extends Controller
 {
@@ -15,5 +17,14 @@ class AuthenticateController extends Controller
     public function register(RegisterRequest $request)
     {
 
+        $data['password'] = bcrypt($request->get('password'));
+        dd($data);
+        $user = User::create($data);
+        $accessToken = $user->createToken('UserToken')->accessToken;
+        return response()->json([
+                                    'user'       => new UserResource($user),
+                                    'token'      => $accessToken,
+                                    'token_type' => 'Bearer'
+                                ]);
     }
 }
