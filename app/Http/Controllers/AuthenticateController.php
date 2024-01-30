d18a8caab6286041fbb2793d4ce8f2ecb89f2fff
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 17:15:06 +0330

    ref: register()

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -20,7 +20,7 @@ class AuthenticateController extends Controller
         return $this->respondWithToken($token);
     }
 
-    public function register(RegisterRequest $request)
+    public function register(RegisterRequest $request): JsonResponse
     {
         $data = $request->validated();
         $data['password'] = bcrypt($request->get('password'));
@@ -28,12 +28,20 @@ class AuthenticateController extends Controller
         /** @var User $user */
         $user = User::query()->create($data);
 
-        $accessToken = $user->createToken('UserToken');
+        if (!$token = auth()->attempt(['email' => $request->get('email'), 'password' => $request->get('password')])) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
 
+        return $this->respondWithToken($token);
+
+    }
+
+    protected function respondWithToken($token): JsonResponse
+    {
         return response()->json([
-            'user'       => $user->toArray(),
-            'token'      => $accessToken,
-            'token_type' => 'Bearer'
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => auth()->factory()->getTTL() * 60
         ]);
     }
 }
