567f82176d2b14251b2088930793ef98c621f552
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 09:49:41 +0330

    feat: add rules in RegisterRequest

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -2,27 +2,27 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'name'     => ['required', 'string', 'min:3'],
+            'email'    => ['required', 'email'],
+            'password' => ['required', 'confirmed', 'min:4'],
         ];
     }
 }
