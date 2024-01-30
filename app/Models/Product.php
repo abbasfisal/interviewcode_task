bcfaf6034ec5118ccb81d5547a152bbabdced0d0
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 07:13:39 +0330

    feat: add Product Model

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'inventory'
+    ];
+    protected $casts = [
+        'name' => 'string',
+        'price' => 'integer',
+        'inventory' => 'integer'
+    ];
+}
