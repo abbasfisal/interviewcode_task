89e27a9d30017fd5370691ca87dbd92a2a11fce5
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 17:15:23 +0330

    feat: create 2 user in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Product;
+use App\Models\User;
 use Database\Factories\ProductFactory;
 use Illuminate\Database\Seeder;
 
@@ -12,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        User::factory(2)->create();
         Product::factory(10)->create();
 
         // \App\Models\User::factory()->create([
