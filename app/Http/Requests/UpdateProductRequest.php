b4a5a25397c4ad4f86b8c3eacc9e7a377f46a302
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 14:47:40 +0330

    ref: change authorize()

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateProductRequest extends FormRequest
@@ -11,18 +12,20 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'name'      => ['string', 'min:4'],
+            'price'     => ['integer', 'gt:0'],
+            'inventory' => ['integer']
         ];
     }
 }
