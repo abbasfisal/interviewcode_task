d2d8a62422da003b65dc1e0d69eb6d52031f6c95
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 18:43:45 +0330

    feat: add rules to store order request

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreOrderRequest extends FormRequest
@@ -11,18 +12,22 @@ class StoreOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'products'              => ['required', 'array'],
+            'products.*.product_id' => ['required'],
+            'products.*.name'       => ['required'],
+            'products.*.count'      => ['required', 'integer'],
+            'products.*.price'      => ['required', 'integer']
         ];
     }
 }
