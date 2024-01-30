cda503049e1c483e7985e4feb481f89f3523b207
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 18:16:29 +0330

    feat: add fillable, casting, relation to Order Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'products',
+        'total_price'
+    ];
+    protected $casts = [
+        'total_price' => 'integer'
+    ];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
