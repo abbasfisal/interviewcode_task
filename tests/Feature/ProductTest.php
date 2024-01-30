2db4f326b6a3b15b4ad0b6cffcfd18bceeec6050
Abbas Momeni <a.momeni@zoot.com>
2024-01-30 12:55:28 +0330

    feat: write test for store product

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Models\Product;
+use App\Models\User;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
@@ -10,6 +12,7 @@ class ProductTest extends TestCase
     // use RefreshDatabase;
     public function clearProducts(): void
     {
+        User::query()->forceDelete();
         Product::query()->forceDelete();
     }
 
@@ -18,10 +21,28 @@ class ProductTest extends TestCase
         $this->clearProducts();
         Product::factory(10)->create();
 
-        $response = $this->get('/api/products');
+        $response = $this->get(route('products.index'));
 
         $this->assertDatabaseCount(Product::class, 10);
 
         $response->assertStatus(200);
+    }
+
+    public function test_store_validation()
+    {
+        $response = $this->postJson(route('products.store'));
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonMissingValidationErrors(['errors.name', 'errors.price', 'errors.inventory']);
+    }
+
+    public function test_store_product()
+    {
+        $this->clearProducts();
+        $product_data = ['name' => 'product one', 'price' => 20000, 'inventory' => 200];
+
+        $response = $this->postJson(route('products.store'), $product_data);
+        $response->assertStatus(201);
+        $this->assertDatabaseCount(Product::class, 1);
+
     }
 }
