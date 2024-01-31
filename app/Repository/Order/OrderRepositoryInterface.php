d44ed55a9933d1ae95ac2f24be9b73db03f2969d
Abbas Momeni <a.momeni@zoot.com>
2024-01-31 06:42:49 +0330

    feat: add destroy() method to order repository interface

--- app/Repository/Order/OrderRepositoryInterface.php
+++ app/Repository/Order/OrderRepositoryInterface.php
@@ -2,9 +2,14 @@
 
 namespace App\Repository\Order;
 
+use App\Models\Order;
+
 interface OrderRepositoryInterface
 {
     public function store(array $data);
 
     public function index();
+
+    public function destroy(Order $product);
+
 }
