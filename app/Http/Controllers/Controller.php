a33bbdbb63071e7c7f2735031ff517cf0ec629a5
Abbas Momeni <a.momeni@zoot.com>
2024-02-01 15:58:20 +0330

    feat: add swagger doc to controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="Apis List"
+ * )
+ *
+ * @OA\PathItem(path="/api")
+ *
+ *
+ * @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      type="http",
+ *      scheme="bearer"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
