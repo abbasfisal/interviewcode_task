bae360c77330b1c7d62c0e429a34f67f22059609
Abbas Momeni <a.momeni@zoot.com>
2024-02-01 10:36:41 +0330

    feat: add rules to update order request

--- app/Http/Requests/UpdateOrderRequest.php
+++ app/Http/Requests/UpdateOrderRequest.php
@@ -3,26 +3,21 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateOrderRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return Auth::id() == $this->order->user_id;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'products'              => ['required', 'array'],
+            'products.*.product_id' => ['exists:products,_id'],
+            'products.*.count'      => ['integer'],
         ];
     }
 }
